2670b8b485abb71895b9f48d1508c641858c8f9b
marshall-GK <code.multiosop@gmail.com>
2024-01-26 19:15:34 +0530

    Updates

--- src/API/ticketDashboard.api.ts
+++
@@ -1,6 +0,0 @@
-import { GET } from './apiHelper/apiWrapper';
-import { API_BASE_URL, TICKET_ASSIGMENT } from './apiUrl.constants';
-
-export const getTicketAssignments = async () => {
-  return await GET(`${API_BASE_URL}${TICKET_ASSIGMENT}`);
-}
